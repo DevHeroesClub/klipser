2da283a1a3eeb7c7f1913e4348296f4a7b91bb8b
Sameer Rahmani <lxsameer@gnu.org>
2018-02-28 22:03:33 +0000

    deployment script has been added

--- scripts/remote.sh
+++ scripts/remote.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 echo "[REMOTE]: Cleaning up..."
-rm -rf ~/_site
+rm -rf ~/build
 rm -rf ~/sn_devheroes/home
 echo "[REMOTE]: Extracting the package..."
 tar zxf ~/tmp/sn.tar.gz -C ~/tmp/
 echo "[REMOTE]: Installing the content..."
 mkdir -p ~/lxsameer.com
-mv -v ~/tmp/_site ~/sn_devheroes/home
+mv -v ~/tmp/build ~/sn_devheroes/home
 chmod 755 ~/sn_devheroes/ -R
 echo "[REMOTE]: Cleaning up..."
 rm -rf ~/resources ~/tmp/
